3823b9dcecfd31ee397e23d8b857b7486a5e79e1
devemre <dev.emre.aydin@gmail.com>
2024-04-27 15:31:14 +0300

    Part 4: REST CRUD APIs - 3

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="employees")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
